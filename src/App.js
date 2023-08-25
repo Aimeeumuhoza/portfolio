90f2fa9d09a6e9ccbba467a4cf4a642c1850ea7d
Aimeeumuhoza <aimeeumuhoza1@gmail.com>
2023-08-25 09:54:17 +0200

    contact me page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import "./App.css"
 import About from './pages/About/About';
 import Home from './pages/Home/Home';
+import Contact from './pages/Contact/ContactUs'
+import Activities from './pages/What/what';
 
 function App() {
   return (
     <>
       <Home />
       <About />
+     <Activities/>
+     <Contact/>
     </>
   );
 }
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,13 +5,13 @@ const Sidebar = () => {
   return (
       <div class="sidebar">
         <div class="profile-picture">
-          <img src="" />
+          
         </div>
         <h3>Aimee UMUHOZA</h3>
         <ul>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About Me</a></li>
-          <li><a href="#whatIdo">What I do</a></li>
+          <li><a href="#activity">What I do</a></li>
           <li><a href="#resume">Resume</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
